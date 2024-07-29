2aeeff8d890d1f3b981ad13e5430a3c9e1b4a254
juliShmi <juliashmitko@gmail.com>
2024-07-29 18:54:55 +0300

    fix a bug with selectors and add a tap button to show the current score (challenge task)

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var button1: UIButton!
+    
     @IBOutlet var button2: UIButton!
+    
     @IBOutlet var button3: UIButton!
     
     var countries = [String]()
@@ -38,19 +40,21 @@ class ViewController: UIViewController {
         
         askQuestion(action: nil)
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showScore))
+        
     }
+    
     func askQuestion(action: UIAlertAction!) {
         countries.shuffle()
         
         correctAnswer = Int.random(in: 0...2)
-    
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = "Guess \(countries[correctAnswer].uppercased()) \t" + "Score: \(score)"
     }
-    
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
@@ -68,7 +72,7 @@ class ViewController: UIViewController {
         if counter < 10 {
             let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-            present(ac, animated: true)
+            present(ac, animated: true, completion: nil)
         } else {
             dismiss(animated: true, completion: nil)
             
@@ -78,6 +82,12 @@ class ViewController: UIViewController {
             score = 0
             counter = 0
         }
+    }
+    
+    @objc func showScore() {
+        let myScore = UIAlertController(title: "Check score", message: "Your score is \(score)", preferredStyle: .alert)
+        myScore.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(myScore, animated: true)
     }
 }
 
